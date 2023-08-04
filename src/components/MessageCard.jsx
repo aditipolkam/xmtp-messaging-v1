42bedd618ed699c9df3d711945528f0540c49517
Aditi Polkam <aditipolkam@gmail>
2023-08-04 23:55:51 +0530

    decrypt image attachment preview

--- src/components/MessageCard.jsx
+++ src/components/MessageCard.jsx
@@ -1,42 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { shortAddress } from "../utils/utils";
 import { useContext } from "react";
 import { XmtpContext } from "../contexts/XmtpContext";
-import { ContentTypeRemoteAttachment, RemoteAttachmentCodec } from "@xmtp/content-type-remote-attachment";
+import { AttachmentCodec, ContentTypeAttachment, ContentTypeRemoteAttachment, RemoteAttachmentCodec } from "@xmtp/content-type-remote-attachment";
 
-const MessageCard = ({msg}) => {
-  const [providerState] = useContext(XmtpContext);
-  const { client } = providerState;
 
-  const decryptAttachment = async(message) =>{
-    const attachment = await RemoteAttachmentCodec.load(message, client);
-    return attachment
-  }
-  
-  
-  if (msg.contentType.sameAs(ContentTypeRemoteAttachment)) {
-    const attachment = decryptAttachment(msg.content);
-    return(
-      <div>
-        {attachment.fileName}
-      </div> 
-    )
-  }
-
- 
-  return (
-    <>
-      <div className="msg-header flex justify-start">
+const TextView = ({content,msg}) => {
+  return(
+    <div className="msg-header flex justify-start">
         <div className="identicon" />
         <div className="convo-info align-start flex-dir-col flex justify-start">
           <div>
             <b>{shortAddress(msg.senderAddress)}</b>
           </div>
-          <div>{msg.content}</div>
+          <div>{content}</div>
         </div>
       </div>
-    </>
-  );
+  )
+}
+
+const ImageView = ({content}) =>{
+  return(
+    <div>
+        <img
+          onLoad={() => {
+          window.scroll({ top: 10000, behavior: "smooth" });
+          }}
+          src={content}
+          // title={attachment.filename}
+        />
+    </div> 
+  )
+}
+
+const MessageCard = ({msg}) => {
+  const [providerState] = useContext(XmtpContext);
+  const { client } = providerState;
+  const [content, setContent] = useState();
+  const [isAttachment, setIsAttachment] = useState(false);
+
+
+  useEffect(()=>{
+    const getContent = async() => {
+      // console.log(msg)
+      if (msg.contentType.sameAs(ContentTypeRemoteAttachment) || msg.contentType.sameAs(ContentTypeAttachment)) {
+        setIsAttachment(true)
+        let attachment;
+        if (msg.contentType.sameAs(ContentTypeRemoteAttachment)) {
+          attachment = await RemoteAttachmentCodec.load(msg.content, client);
+        }
+        else{
+          attachment = await AttachmentCodec.load(msg.content,client)
+        }
+        
+        console.log(attachment)
+        if(!attachment)
+          return;
+      
+        console.log(attachment)
+        if (attachment.mimeType.startsWith("image/")) {
+          const objectURL = URL.createObjectURL(
+            new Blob([Buffer.from(attachment.data)], {
+              type: attachment.mimeType,
+            })
+          );
+          setContent(objectURL);
+        }
+      }
+      else{
+        setIsAttachment(false)
+        setContent(msg.content)
+      }
+    }
+    getContent();
+
+  },[msg])
+
+  return isAttachment ? <ImageView content={content} /> : <TextView content={content} msg={msg}/>
+ 
 }
 
 export default MessageCard;
--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 import useStreamMessages from "../hooks/useStreamMessages";
 import MessageCard from "./MessageCard";
+import { useContext, useEffect } from "react";
+import { XmtpContext } from "../contexts/XmtpContext";
 
 const MessageList = ({ isNewMsg, convoMessages, selectedConvo }) => {
   useStreamMessages(selectedConvo);
+  const [providerState] = useContext(XmtpContext);
+  const { client } = providerState;
+
+  // useEffect(()=>{
+  //   const getMessages = async() =>{
+  //     const conversation = await client.conversations.newConversation(selectedConvo)
+  //       const opts = {
+  //         // Only show messages from last 24 hours
+  //         startTime: new Date(new Date().setDate(new Date().getDate() - 7)),
+  //         endTime: new Date(),
+  //       };
+  //       const messagesInConversation = await conversation.messages(opts);
+  //       console.log(messagesInConversation)
+  //   }
+  //   getMessages();
+  // },[])
   
   return (
     <div className="msgs-container flex flex-dir-col">
       <div className="mt-auto">
         {!isNewMsg &&
           convoMessages.map((msg, index) => {
-            console.log(index, msg)
+            
             return <MessageCard key={msg.id} msg={msg} />;
           })}
       </div>
